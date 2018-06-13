1166b094210dbbbc98728aa59d864405d94586ea
Daniel Loughry <loughry_d@yahoo.com>
2018-06-13 10:25:06 -0400

    init commit

--- MathProblems/Program.cs
+++ MathProblems/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathProblems
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace MathProblems{
+
+	public class Program { 
+
+		static int Add(string nbrs)		{
+			var strArray = nbrs.Split(',');
+			List<int> ints = new List<int>();
+			foreach(var str in strArray) {
+				int i;
+				int.TryParse(str, out i);
+				ints.Add(i);
+			}
+			var sum = ints.Sum();
+			return sum;
+
+
+		}			
+	
+		static void Main(string[] args)		{
+
+			Console.WriteLine($"Enter the numbers you would like to add shit pirate:");
+			string response = Console.ReadLine();
+			var answer = Add(response);
+			Console.WriteLine($"{response} is {answer}");
+			
+
+
+
+
+
 		}
 	}
 }
